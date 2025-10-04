c3ee6d1c982d300966a791c05ffbde81da59861e
N4-H4 <nahamiriyala@gmail.com>
2025-10-04 20:01:48 +0530

    rock-paper-scissors main

--- scripts/lets-play.js
+++ scripts/lets-play.js
@@ -1,6 +1,5 @@
 let computer;
 
-
 let score = JSON.parse(localStorage.getItem('rpsScore')) || {
   playerScore : 0,
   computerScore : 0,
@@ -14,44 +13,23 @@ if(!score) {
     Ties : 0
   }
 }
-  let resume = document.querySelector('.js-resume');
-  
-let outcome = (playerMove, computer) => {
-  if(playerMove === 'rock' && computer === 'rock' || playerMove === 'paper' && computer === 'paper' || playerMove === 'scissors' && computer === 'scissors'){
-    document.querySelector('.js-outcome').innerHTML = `Tie.`
-    score.Ties += 1;
-  } else if(playerMove === 'rock' && computer === 'paper' || playerMove === 'paper' && computer === 'scissors' || playerMove === 'scissors' && computer === 'rock') {
-    document.querySelector('.js-outcome').innerHTML = `You lose.`
-    score.computerScore += 1;
-    
-  } else if(playerMove === 'paper' && computer === 'rock' || playerMove === 'scissors' && computer === 'paper' || playerMove === 'rock' && computer === 'scissors') {
-    document.querySelector('.js-outcome').innerHTML = `You win!`
-    score.playerScore += 1;
-  }
-
-  if(resume) {
-  document.querySelector('.js-score').innerHTML = `Your Score: ${score.playerScore} Computer: ${score.computerScore} Ties: ${score.Ties}`
-  } else if(!resume) {
-    document.querySelector('.js-score').innerHTML = `Your Score: ${score.playerScore} Computer: ${score.computerScore} Ties: ${score.Ties}`
-  }
-}
 
 function playGame(playerMove) {
   
   if(playerMove === 'rock') 
     {
-      document.querySelector(".js-picked").innerHTML = `You picked ${playerMove} Computer picked ${computer}`
     computerMove();
+    document.querySelector(".js-picked").innerHTML = `You <img src="./rps-images-files/${playerMove}-emoji.png" class="picked"><img src="./rps-images-files/${computer}-emoji.png" class="picked"> Computer`;
     outcome(playerMove, computer);
   } 
   else if(playerMove === 'paper') {
-    document.querySelector(".js-picked").innerHTML = `You picked ${playerMove} Computer picked ${computer}`
     computerMove();
+    document.querySelector(".js-picked").innerHTML = `You <img src="./rps-images-files/${playerMove}-emoji.png" class="picked"><img src="./rps-images-files/${computer}-emoji.png" class="picked"> Computer`;
     outcome(playerMove, computer);
   } 
   else if(playerMove === 'scissors') {
-    document.querySelector(".js-picked").innerHTML = `You picked ${playerMove} Computer picked ${computer}`
     computerMove();
+    document.querySelector(".js-picked").innerHTML = `You <img src="./rps-images-files/${playerMove}-emoji.png" class="picked"><img src="./rps-images-files/${computer}-emoji.png" class="picked"> Computer`;
     outcome(playerMove, computer);
   }
   localStorage.setItem('rpsScore', JSON.stringify(score));
@@ -72,6 +50,21 @@ let randomNumber = Math.random();
     computer = 'scissors';
     
   }
+}
+
+let outcome = (playerMove, computer) => {
+  if(playerMove === 'rock' && computer === 'rock' || playerMove === 'paper' && computer === 'paper' || playerMove === 'scissors' && computer === 'scissors'){
+    document.querySelector('.js-outcome').innerHTML = `Tie.`
+    score.Ties += 1;
+  } else if(playerMove === 'rock' && computer === 'paper' || playerMove === 'paper' && computer === 'scissors' || playerMove === 'scissors' && computer === 'rock') {
+    document.querySelector('.js-outcome').innerHTML = `You lose.`
+    score.computerScore += 1;
+    
+  } else if(playerMove === 'paper' && computer === 'rock' || playerMove === 'scissors' && computer === 'paper' || playerMove === 'rock' && computer === 'scissors') {
+    document.querySelector('.js-outcome').innerHTML = `You win!`
+    score.playerScore += 1;
+  }
+   document.querySelector('.js-score').innerHTML = `You: ${score.playerScore}    Computer: ${score.computerScore}    Ties: ${score.Ties}`;
 }
 
 function restartGame() {
